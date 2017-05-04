30e83857a8129d11ba3b4635359b7aaaaf6e4121
BAAMOSK <bunth667@gmail.com>
2017-05-03 22:40:18 -0400

    updated dependencies

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const logger = require('morgan');
+
+app.use(logger('combined'));
+
+app.get('/', (req, res) => {
+  res.json('Hello');
+});
 
 app.listen(3000, err => {
   if(err) throw err;
